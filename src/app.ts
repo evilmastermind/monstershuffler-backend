b0dc3bda8f59f77ee9caa8bd687415ee730326fe
Ismaele Gobbo <ismaele.gobbo@silla.industries>
2023-02-14 12:57:21 +0100

    routes description

--- src/app.ts
+++ src/app.ts
@@ -121,10 +121,21 @@ async function main() {
           tags: [
             { name: 'users', description: 'The users\' account. ' },
             { name: 'actions', description: 'Retrieve the official actions of srd monsters, classes and races to build your creatures faster. Save your custom actions and share them with other users.' },
+            { name: 'armor', description: ''},
+            { name: 'backgrounds', description: 'Tiny snippets of lore to add to your characters for roleplay purposes.' },
+            { name: 'characters', description: 'Any type of Dungeons & Dragons creature, like Monsters, NPCs, Player Characters, etc.' },
+            { name: 'classes', description: 'Classes are the main way to define a character\'s role in the game. They define what abilities the character has and how they can use them.' },
             { name: 'class variants', description: 'Class variants can be subclasses, archetypes, variants or customizations that add new features on top of their base class.' },
+            { name: 'damage types', description: 'Damage types are the different types of damage that can be dealt by weapons, spells, and other effects.' },
+            { name: 'folders', description: 'Folders are a way to organize your characters, actions, etc. into groups.' },
+            { name: 'languages', description: 'Languages are the different ways that characters can communicate with each other.' },
+            { name: 'names', description: 'A collection of names for your characters, divided into different categories.' },
+            { name: 'professions', description: 'Professions are a way to define a character\'s occupation, like a soldier, a merchant, a priest, etc. and give them minor abilities related to that occupation. They are usually given to NPCs in place of classes.' },
+            { name: 'races', description: 'A race defines the innate abilities derived from a character\'s fantasy ancestry.' },
+            { name: 'race variants', description: 'Race variants, or subraces, further define a character\'s ancestry by adding new features on top of their base race.' },
+            { name: 'templates', description: 'Templates are \'bundles of statistics\' that can be added to characters to turn them into something different, like a Werefolf, a Zombie, or a Fire creature.' },
           ],
         }
-
       })
     );
     server.register(swaggerUi, {
