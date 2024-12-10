4c7a29e85359b7d80976ab2cbf0ffda89fea6329
Ismaele Gobbo <ismaele.gobbo@silla.industries>
2024-12-10 07:54:00 +0100

    fix: openai default models
    Set OpenAI default model to  gpt-4o-mini

--- modules/ai/ai.service.ts
+++ modules/ai/ai.service.ts
@@ -7,7 +7,7 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
  * Generate text using the OpenAI API.
  * Available models: https://openai.com/api/pricing/
  **/
-export async function generateTextStream(prompt: string, model = 'gpt-3.5-turbo-0125', temperature = 1) {
+export async function generateTextStream(prompt: string, model = 'gpt-4o-mini', temperature = 1) {
   return await openai.chat.completions.create({
     messages: [{ role: 'system', content: prompt,  }],
     model: model,
@@ -16,7 +16,7 @@ export async function generateTextStream(prompt: string, model = 'gpt-3.5-turbo-
   });
 }
 
-export async function generateText(prompt: string, model = 'gpt-3.5-turbo-0125', temperature = 1) {
+export async function generateText(prompt: string, model = 'gpt-4o-mini', temperature = 1) {
   return await openai.chat.completions.create({
     messages: [{ role: 'system', content: prompt,  }],
     model: model,
